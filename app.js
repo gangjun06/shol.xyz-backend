883e07f4aa1416da111748c036b5e93e8f12331f
이강준 <admin@gangjun.dev>
2020-04-30 21:12:36 +0900

    wa! i can use ejs!

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const { config } = require("./config");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-var router = require("./router/main")(app);
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
+// settings
+app.set('views', __dirname+'/views')
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+
+// 망할 cross origin
 app.use(cors());
+
+// router 설정
+require("./router/main")(app);
 
 app.listen(config.devPORT, () => {
   console.log(`App listening on ${config.devPORT}`);
--- router/main.js
+++ router/main.js
@@ -1,8 +1,5 @@
 module.exports = (app) => {
   app.get("/", (req, res) => {
-    res.send("a");
-  });
-  app.get("/docs", (req, res) => {
-    res.send("a");
+      res.render('index')
   });
 };
