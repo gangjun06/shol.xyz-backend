e95a8a24a2ccdc690c00c7c7a17d82227f22f8da
이강준 <admin@gangjun.dev>
2020-05-01 18:57:47 +0900

    c o m m i t

--- router/main.js
+++ router/main.js
@@ -1,52 +1,68 @@
-const Url = require("../models/url");
+const Url = require('../models/url')
+const path = require('path')
 
-const ShortUniquedId = require("short-unique-id").default;
-const uid = new ShortUniquedId();
+const ShortUniquedId = require('short-unique-id').default
+const uid = new ShortUniquedId()
 
 module.exports = (app) => {
-  app.get("/", (req, res) => {
-    res.render("index");
-  });
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  })
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  })
 
-  app.post("/", (req, res) => {
-    let url = req.query.url;
+  app.post('/', (req, res) => {
+    let url = req.query.url
 
     if (!url) {
-      res.writeHead(500);
+      res.writeHead(500, {})
+      res.end()
+      return
     }
 
-    if (!url.startsWith("http://") && !url.startsWith("https://")) {
-      url = "http://" + String(url);
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      url = 'http://' + String(url)
     }
 
     Url.findOne({ longurl: url }, (err, theUrl) => {
-      if (err) res.writeHead(500);
+      if (err) {
+        console.log(err)
+        res.writeHead(500)
+        res.end()
+        return
+      }
 
       if (theUrl) {
-        res.json({ short: theUrl.short });
+        res.json({ short: theUrl.short })
+        return
       } else {
-        let id = uid.randomUUID(5);
+        let id = uid.randomUUID(5)
         Url({
           longurl: url,
           short: id,
-        }).save();
-        res.json({ short: id });
+        }).save()
+        res.json({ short: id })
+        return
       }
-    });
-  });
+    })
+  })
 
-  app.get("/:urlCode", (req, res) => {
-    var url = String(req.params.urlCode);
+  app.get('/:urlCode', (req, res) => {
+    var url = String(req.params.urlCode)
 
     Url.findOne({ short: url }, (err, theUrl) => {
-      if (err) res.writeHead(500);
+      if (err) {
+        res.writeHead(500)
+        res.end()
+      }
 
       if (theUrl) {
-        res.writeHead(301, { Location: String(theUrl.longurl) });
-        res.end();
+        res.writeHead(301, { Location: String(theUrl.longurl) })
+        res.end()
       } else {
-        res.render("notfound");
+        res.render('notfound')
       }
-    });
-  });
-};
+    })
+  })
+}
