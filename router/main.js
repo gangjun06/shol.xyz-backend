aadf1335931c3f04282de91c078acf5c79c74235
이강준 <admin@gangjun.dev>
2020-05-17 21:13:48 +0900

    update text

--- router/main.js
+++ router/main.js
@@ -8,9 +8,7 @@ module.exports = (app) => {
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-  app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+  
 
   /**
    * @swagger
